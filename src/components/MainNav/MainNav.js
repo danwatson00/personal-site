6da381281e833f3487d8ffa276aef8decdca3dce
Dan Watson <danwatson00@yahoo.com>
2019-02-06 22:27:46 -0600

    got basic sidebar implemented

--- src/components/MainNav/MainNav.js
+++ src/components/MainNav/MainNav.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import './MainNav.css';
 
 
@@ -8,8 +7,9 @@ class MainNav extends Component {
     return ( 
       <div className="mainNav">
         <ul>
-          <li> <a href="/">HOME</a></li>
+          <li> <a href="/">PORTFOLIO</a></li>
           <li><a href="/about.html">ABOUT</a></li>
+          <li><a href="/resume.html">RESUME</a></li>
           <li><a href="/contact.html">CONTACT</a></li>
         </ul>
       </div>
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import MainNav from '../MainNav/MainNav'
+import initialsLogo from '../../images/initials-logo.png'
+import './Sidebar.css'
 
 const Sidebar = ({ menu, logo }) =>
-  <div> 
+  <div className="sidebar"> 
+    <img className="initialsLogo" src={initialsLogo} alt={"Dan Watson Logo"}/>
     <MainNav />
   </div>
 
